ec22ce2c6aa1cae0950370c23992d7bc9cb8c292
Markus Falk <mail@markus-falk.com>
2020-12-10 17:02:49 +0100

    feat: add rate limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import * as rateLimit from 'express-rate-limit';
+
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    rateLimit({
+      windowMs: 60 * 60 * 1000,
+      max: 5000,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();

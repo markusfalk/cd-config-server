fe38a94cea37ac4ad2ff2a017ae9d809959490b9
Markus Falk <mail@markus-falk.com>
2020-12-23 17:47:51 +0100

    refactor: move semver logic to own service

---
+++ src/semantic-versioning/semantic-versioning.service.spec.ts
@@ -0,0 +1,165 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { Config } from '../_interfaces/config.interface';
+import { errorsMessages, SemanticVersioningService } from './semantic-versioning.service';
+
+describe('SemanticVersioningService', () => {
+  let service: SemanticVersioningService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SemanticVersioningService],
+    }).compile();
+
+    service = module.get<SemanticVersioningService>(SemanticVersioningService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should match specific version', () => {
+    const configs: Config[] = [
+      { compatibleWithAppVersion: '1.0.0' },
+      { compatibleWithAppVersion: '2.0.0' },
+      { compatibleWithAppVersion: '3.0.0' },
+    ];
+
+    service.findMatchingFile(configs, '1.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[0]);
+    });
+
+    service.findMatchingFile(configs, '2.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[1]);
+    });
+
+    service.findMatchingFile(configs, '3.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[2]);
+    });
+  });
+
+  it('should check config attribute', () => {
+    const configs: any[] = [{ wrongKey: '1.0.0' }];
+
+    service.findMatchingFile(configs, '1.0.0').subscribe(
+      () => console.log(),
+      (error) => {
+        expect(error).toBe(errorsMessages.wrongKey);
+      },
+    );
+  });
+
+  it('should throw error on empty results', () => {
+    const configs: Config[] = [{ compatibleWithAppVersion: '1.0.0' }];
+
+    service.findMatchingFile(configs, '2.0.0').subscribe(
+      () => console.log(),
+      (error) => {
+        expect(error).toBe(errorsMessages.noConfig + ` 2.0.0`);
+      },
+    );
+  });
+
+  it('should match ^', () => {
+    const configs: Config[] = [
+      { compatibleWithAppVersion: '^2.0.0' }, // 0
+      { compatibleWithAppVersion: '^2.1.0' }, // 1
+      { compatibleWithAppVersion: '^2.1.1' }, // 2
+
+      { compatibleWithAppVersion: '^1.2.1' }, // 3
+      { compatibleWithAppVersion: '^1.1.0' }, // 4
+      { compatibleWithAppVersion: '^1.3.1' }, // 5
+      { compatibleWithAppVersion: '^1.0.0' }, // 6
+      { compatibleWithAppVersion: '^1.2.0' }, // 7
+      { compatibleWithAppVersion: '^1.1.1' }, // 8
+
+      { compatibleWithAppVersion: '^3.1.1' }, // 9
+      { compatibleWithAppVersion: '^3.0.0' }, // 10
+      { compatibleWithAppVersion: '^3.1.0' }, // 11
+    ];
+
+    service.findMatchingFile(configs, '1.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[6]);
+    });
+
+    service.findMatchingFile(configs, '1.1.0').subscribe((response) => {
+      expect(response).toEqual(configs[4]);
+    });
+
+    service.findMatchingFile(configs, '2.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[0]);
+    });
+
+    service.findMatchingFile(configs, '2.0.1').subscribe((response) => {
+      expect(response).toEqual(configs[0]);
+    });
+
+    service.findMatchingFile(configs, '3.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[10]);
+    });
+
+    service.findMatchingFile(configs, '3.0.1').subscribe((response) => {
+      expect(response).toEqual(configs[10]);
+    });
+
+    service.findMatchingFile(configs, '3.1.1').subscribe((response) => {
+      expect(response).toEqual(configs[9]);
+    });
+  });
+
+  it('should match greater than', () => {
+    const configs: Config[] = [
+      { compatibleWithAppVersion: '>1.0.0' },
+      { compatibleWithAppVersion: '>1.1.0' },
+      { compatibleWithAppVersion: '>1.1.1' },
+      { compatibleWithAppVersion: '>2.0.0' },
+    ];
+
+    service.findMatchingFile(configs, '1.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[3]);
+    });
+  });
+
+  it('should match lower than', () => {
+    const configs: Config[] = [
+      { compatibleWithAppVersion: '<1.0.0' },
+      { compatibleWithAppVersion: '<1.1.0' },
+      { compatibleWithAppVersion: '<1.1.1' },
+      { compatibleWithAppVersion: '<2.0.0' },
+      { compatibleWithAppVersion: '<3.0.0' },
+    ];
+
+    service.findMatchingFile(configs, '1.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[3]);
+    });
+  });
+
+  it('should match lower than + equal', () => {
+    const configs: Config[] = [
+      { compatibleWithAppVersion: '<=1.0.0' },
+      { compatibleWithAppVersion: '<=1.1.0' },
+      { compatibleWithAppVersion: '<=1.1.1' },
+      { compatibleWithAppVersion: '<=2.0.0' },
+      { compatibleWithAppVersion: '<=3.0.0' },
+    ];
+
+    service.findMatchingFile(configs, '1.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[3]);
+    });
+  });
+
+  it('should match ~', () => {
+    const configs: Config[] = [
+      { compatibleWithAppVersion: '~1.0.0' },
+      { compatibleWithAppVersion: '~2.0.0' },
+    ];
+
+    service.findMatchingFile(configs, '1.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[0]);
+    });
+
+    service.findMatchingFile(configs, '2.0.0').subscribe((response) => {
+      expect(response).toEqual(configs[1]);
+    });
+  });
+});
---
+++ src/semantic-versioning/semantic-versioning.service.ts
@@ -0,0 +1,65 @@
+import { of, throwError } from 'rxjs';
+import { satisfies } from 'semver';
+import { Config } from 'src/_interfaces/config.interface';
+
+import { Injectable } from '@nestjs/common';
+
+
+
+// export interface SemanticError {
+//   appversion: string;
+//   code: number;
+//   message: string;
+// }
+
+// const wrongKeyError: SemanticError = {
+//   appversion: ${appVersion},
+//   code: 400,
+//   message:
+// }
+
+export const enum errorsMessages {
+  wrongKey = `The config file must include the key 'compatibleWithAppVersion'`,
+  noConfig = `No config found matching`,
+}
+
+@Injectable()
+export class SemanticVersioningService {
+  /*
+   * Checks the config attribute 'compatibleWithAppVersion' in an array of configs
+   * then returns the highest matching config.
+   */
+  findMatchingFile(configFiles: Config[], appVersion: string) {
+    let error = false;
+
+    const matched = configFiles.filter((file) => {
+      if (file.compatibleWithAppVersion) {
+        return satisfies(appVersion, file.compatibleWithAppVersion);
+      } else {
+        error = true;
+      }
+    });
+
+    if (error) {
+      return throwError(errorsMessages.wrongKey);
+    }
+
+    // need to sort to use last entry as highest matching version
+    if (matched.length > 1) {
+      matched.sort((a, b) => {
+        return ('' + a.compatibleWithAppVersion).localeCompare(
+          b.compatibleWithAppVersion,
+        );
+      });
+    }
+
+    // return last value in case more than one config matches the appversion
+    const last = matched[matched.length - 1];
+
+    if (last) {
+      return of(last);
+    } else {
+      return throwError(errorsMessages.noConfig + ` ${appVersion}`);
+    }
+  }
+}
